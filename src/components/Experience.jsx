10a587134cdccb1b142c3966beaa3d3861fadb0a
Shay <shayphs@gmail.com>
2023-04-19 17:48:42 +0300

    start of experience section

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,7 +1,23 @@
-import React from "react";
+import { motion } from "framer-motion";
+import {
+  VerticalTimeline,
+  VerticalTimelineElement,
+} from "react-vertical-timeline-component";
+import "react-vertical-timeline-component/style.min.css";
+import { styles } from "../styles";
+import { experiences } from "../contants";
+import { SectionWrapper } from "../hoc";
+import { textVariant } from "../utils/motion";
 
 const Experience = () => {
-  return <div>Experience</div>;
+  return (
+    <>
+      <motion.div variants={textVariant()}>
+        <p className={styles.sectionSubText}>What I have done so far</p>
+        <h2 className={styles.sectionHeadText}>Work Experience.</h2>
+      </motion.div>
+    </>
+  );
 };
 
-export default Experience;
+export default SectionWrapper(Experience, "work");
